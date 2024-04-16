258d33e2cd72eb210339e1f09d31b1c0bab4994c
arief_wicaksono <62870120+wicakson0@users.noreply.github.com>
2024-04-16 13:18:21 +0700

    lemme just save all the failed iteration of this model

---
+++ experiments/autogluon_demo.py
@@ -0,0 +1,14 @@
+from autogluon.multimodal import MultiModalPredictor
+
+import os
+import time
+
+from autogluon.core.utils.loaders import load_zip
+
+zip_file = "https://automl-mm-bench.s3.amazonaws.com/object_detection_dataset/tiny_motorbike_coco.zip"
+download_dir = "./tiny_motorbike_coco"
+
+load_zip.unzip(zip_file, unzip_dir=download_dir)
+data_dir = os.path.join(download_dir, "tiny_motorbike")
+train_path = os.path.join(data_dir, "Annotations", "trainval_cocoformat.json")
+test_path = os.path.join(data_dir, "Annotations", "test_cocoformat.json")
